4130e469d84bb8bce4221965f38bd9c1b4caf898
SaadHussein <sa3dhusseein@gmail.com>
2024-11-17 09:59:59 +0200

    continue working on Foodies Project

---
+++ Foodies App/app/meals/error.js
@@ -0,0 +1,8 @@
+"use client";
+
+export default function Error({ error }) {
+    return <main>
+        <h1>An error occurred!</h1>
+        <p>Failed to detch meal data. Please try again later.</p>
+    </main>;
+}
